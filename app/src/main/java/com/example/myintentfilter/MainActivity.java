87787c7bfd10e77c270a410458e1e6caa54f97a5
roshanjena4 <roshanjena464@gmail.com>
2024-02-25 10:12:59 +0530

    first commit

--- app/src/main/java/com/example/myintentfilter/MainActivity.java
+++ app/src/main/java/com/example/myintentfilter/MainActivity.java
@@ -28,25 +28,22 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        btnNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                pName = name.getText().toString();
-                pMail = mail.getText().toString();
-                pSub= sub.getText().toString();
-                pClass = sclass.getText().toString();
-                pPhone = Double.parseDouble(phone.getText().toString());
+        btnNext.setOnClickListener(view -> {
+            pName = name.getText().toString();
+            pMail = mail.getText().toString();
+            pSub= sub.getText().toString();
+            pClass = sclass.getText().toString();
+            pPhone = Double.parseDouble(phone.getText().toString());
 
-                Intent dataIntent = new Intent(MainActivity.this,StudInfo.class);
+            Intent dataIntent = new Intent(MainActivity.this,StudInfo.class);
 
-                dataIntent.putExtra("name",pName);
-                dataIntent.putExtra("mail",pMail);
-                dataIntent.putExtra("subject",pSub);
-                dataIntent.putExtra("class",pClass);
-                dataIntent.putExtra("phone",pPhone);
+            dataIntent.putExtra("name",pName);
+            dataIntent.putExtra("mail",pMail);
+            dataIntent.putExtra("subject",pSub);
+            dataIntent.putExtra("class",pClass);
+            dataIntent.putExtra("phone",pPhone);
 
-                startActivity(dataIntent);
-            }
+            startActivity(dataIntent);
         });
     }
         public void clr(View v) {
